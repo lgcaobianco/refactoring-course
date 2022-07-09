20f591e080e1bbd1da4fa6ef39a5e3ead7ba5e64
Luiz G. Caobianco <gu.caobianco@gmail.com>
2022-07-09 13:36:51 -0300

    added test for valid product

--- assignment-2/test/product-size.test.ts
+++ assignment-2/test/product-size.test.ts
@@ -10,5 +10,15 @@ describe('test products sizes', () => {
             length: 10
         } as Dimension;
         expect(() => new Product(25.59, dimensions)).toThrowError("Invalid dimensions")
+    });
+
+    it('should create a new product if dimension is valid', () => {
+        const dimensions = {
+            width: 10,
+            height: 10,
+            length: 10
+        };
+        const product = new Product(10, dimensions);
+        expect(product).toBeInstanceOf(Product);
     })
 });
